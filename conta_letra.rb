a04954d249d9bd069e91dce16f1c56b4ea877ce0
alanmzz <alanmzz@pm.me>
2021-03-29 18:20:39 -0300

    melhorias no codigo dos desafios e testes

---
+++ conta_letra.rb
@@ -0,0 +1,30 @@
+phrase = "Ada Lovelace criou o primeiro algoritmo para ser interpretado por máquinas"
+letras = phrase.split('')
+vogais = ["a", "e", "i", "o", "u"]
+conta_vogal = 0
+
+     #if phrase.include?("a" || "e" || "i" || "o" || "u")
+      # conta_vogal += 1
+     #end
+
+     #while letras.include?(vogais)
+        #conta_vogal += 1
+        #final_letras = conta_vogal
+     #end
+    #puts final_letras
+
+ 
+         # phrase.downcase.split('').each do |letra|
+          #  if letra == 'a' || letra == 'á' || letra == 'ã' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' || letra == 'y'
+           #   conta_vogal += 1
+            #end
+          #end
+            #puts conta_vogal
+puts letras.include?(vogais)          
+
+
+
+
+
+
+
