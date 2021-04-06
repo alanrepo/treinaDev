c4438c721ff41c96b1814d6f3ae938984ba71da6
alanmzz <alanmzz@pm.me>
2021-04-06 04:39:27 -0300

    glooooria gloriaaa aleluiaaaa

--- updowncase.rb
+++ updowncase.rb
@@ -1,43 +1,30 @@
 frase = "todo santo dia"
 letras = frase.split('')
-conta = 0
 
 
+letras.each_with_index {|l,i| 
 
-puts letras.fetch(2).upcase
-#letras.each_index {|index|  puts "#{index} #{letras[index]}".upcase }
-puts
-puts
-for l in letras do
-    puts l
-end
-puts
-letras.each do  |l|   
-    puts l.upcase
-end
-puts
-
-
-letras.each_with_index {|val, id|  puts id}
-
-fofos = letras.select {|c| if id.odd? then c.upcase end  }
-#puts fofos
-
-
-#puts conta.odd?
-
-#letras.each_index {|index| conta +=1 if index.even? == true }
-#puts conta
-
-#arr.each {|a| print a -= 10, " "}
-
-#letras.each {|l| if letras.each_index.even?==true then letras.fetch(l).upcase end}
-
-#letras.each_index {|letra| if letra.odd? == true then letras.to_s.downcase end; if letra.even? == true then letra.to_s.upcase end }
-#puts letra
-#puts " #{letras[0].downcase} #{letras[1].upcase} #{letras[2].downcase} #{letras[3].upcase} "
-
+    
+    #for i.odd? do l.upcase end
+    if i.odd?
+        l = l.upcase
+    end
+    # puts "#{l}#{i.odd?}"
+    puts "#{l}"
+}
  
 
+#
+#novos = letras.values_at(* letras.each_index.select {|i| if i.odd? then i +=-10 end})
 
 
+#novos = letras.values_at(* letras.each_index.select {|i| i.even?})
+
+#novos = letras.values_at(* letras.each_index.select {|i| i.even?})
+
+#novos = letras.values_at(* letras.each_index.select {|i| i.even?})
+
+#quero pegar o array letras
+#escolher os valores dos indices pares
+#e converter eles para upcase
+#e imprimir o array com a alteração realizada
